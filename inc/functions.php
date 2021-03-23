bf629fb82cbedb8aa87af6f151ebed7a671233cb
Jyri Ahola <n0ahjy00@students.oamk.fi>
2021-03-23 19:00:46 +0200

    This is a test commit

--- inc/functions.php
+++ inc/functions.php
@@ -8,5 +8,6 @@ function returnError(PDOException $pdoex) {
     echo header('HTTP/1.1 500 Internal Server Error');
     $error = array('error' => $pdoex->getMessage());
     echo json_encode($error);
+    print ('test');
     exit;
 }
