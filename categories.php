032a8687b68938b7aff86bb0a34f9a8d3c68ce16
Aleksi Nokela <n0noal00@student.oamk.fi>
2021-04-06 12:26:56 +0300

    muokattu categories.php:n muuttujia

--- categories.php
+++ categories.php
@@ -5,18 +5,17 @@ require_once './inc/headers.php';
 try
 {
     if(isset($_GET['trnro'])){
-        $id = $_GET['trnro'];
+        $id = filter_input(INPUT_GET, 'trnro', FILTER_SANITIZE_STRING);
         $db = openDb();
         selectAsJson($db,"SELECT * FROM tuote where trnro = $id");
     }
 
     if(isset($_GET['trnimi'])){
-        $id = $_GET['trnimi'];
+        $id = filter_input(INPUT_GET, 'trnimi', FILTER_SANITIZE_STRING);
         $db = openDb();
         selectAsJson($db, "SELECT trnimi FROM tuoteryhma where trnro = $id");
 
     }
-
 
 }
 catch(PDOException $pdoex){
