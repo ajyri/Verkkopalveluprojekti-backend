aa69052bcec19ee7074785555644ea85ab31b6be
ukko hansen-haug <n0hauk00@students.oamk.fi>
2021-04-26 10:56:32 +0300

    Muiden juomien ja teen järsetyksen vaihto

--- verkkopalvelu.sql
+++ verkkopalvelu.sql
@@ -1,7 +1,7 @@
 create DATABASE kahvila;
 use kahvila;
 
-drop table if exists tuoteryhma, tuote;
+drop table if exists tuoteryhma, tilausrivi, tilaus, tuote;
 
 create table tuoteryhma (
 trnro int primary key AUTO_increment,
@@ -17,17 +17,17 @@ trnro int,
 FOREIGN KEY(trnro) REFERENCES tuoteryhma(trnro)
 );
 
-insert into tuoteryhma(trnimi) values ('Kahvit'),('Muut juomat'),('Teet'),('Leivokset');
+insert into tuoteryhma(trnimi) values ('Kahvit'),('Teet'),('Muut juomat'),('Leivokset');
 -- Uudet tuotteet lisätty tuonne sekaan. Kannattaa poistaa tuote-taulu, luoda se uudelleen ja ajaa nää tuotteet kaikki kerralla :)
 insert into tuote(tuotenimi, kuvaus, hinta, trnro) values ('Kahvi', 'Musta kahvi.', 1.50,1),
 ('Espresso', 'Espresso kahvi.', 3, 1),('Cappuccino', 'Espressoa, kuumaa maitoa ja maitovaahto.',3.70,1),
 ('Latte','1/4 Espressoa ja 3/4 maitovaahtoa.',4.10,1),('Americano','Espressoa ja kuumaa vettä.',2.50,1),
-('Jäälatte','Jääpaloilla höystetty latte.',2.50,1),('Earl Grey', 'Maustettu musta tee.', 1, 3),
-('Kamomillatee', 'Rauhoittava tee.', 2, 3),('Vihreä tee', 'Maailman suosituin teelaji.',1.50, 3),
-('Valkoinen tee', 'Vähiten käsitelty ja luonnonmukaisin.', 2.20, 3), ('Oolong','Osittain hapatettu tee.',3.10,3),
-('Rooibos', 'Kafeiiniton yrttitee.', 2.50, 3),('Kaakao', 'Suklaakaakao.',2,2), ('Jäävesi', 'Kylmä vesi.', 0.2, 2),
-('Jaffa', 'Appelsiinilimsa.', 3, 2),('Cola', 'Colanmakuinen limsa.', 3, 2),('Omenamehu', 'Omenantuoremehu.', 3, 2),
-('Appelsiinimehu', 'Appelsiinituoremehu.', 3, 2),('Brownie', 'Suklaaleivos.',3.35,4),
+('Jäälatte','Jääpaloilla höystetty latte.',2.50,1),('Earl Grey', 'Maustettu musta tee.', 1, 2),
+('Kamomillatee', 'Rauhoittava tee.', 2, 2),('Vihreä tee', 'Maailman suosituin teelaji.',1.50, 2),
+('Valkoinen tee', 'Vähiten käsitelty ja luonnonmukaisin.', 2.20, 2), ('Oolong','Osittain hapatettu tee.',3.10,2),
+('Rooibos', 'Kafeiiniton yrttitee.', 2.50, 2),('Kaakao', 'Suklaakaakao.',2, 3), ('Jäävesi', 'Kylmä vesi.', 0.2, 3),
+('Jaffa', 'Appelsiinilimsa.', 3, 3),('Cola', 'Colanmakuinen limsa.', 3, 3),('Omenamehu', 'Omenantuoremehu.', 3, 3),
+('Appelsiinimehu', 'Appelsiinituoremehu.', 3, 3),('Brownie', 'Suklaaleivos.',3.35,4),
 ('Viineri','Vaniljan ja mansikan makuinen leivos.',2.7,4),('Chocolate Chip Cookie','Suklaahippukeksi.',3,4),
 ('Pääsiäisleivos','Keväisesti koristeltu leivos.',4.20,4),('Berliininmunkki','Kuorrutettu hillomunkki.',3.10,4),
 ('Voisilmäpulla','Perinteinen voisilmäpulla',2.20,4);
@@ -127,4 +127,9 @@ salasana varchar (255) NOT NULL);
 INSERT INTO admin_kayttaja (kayttajanimi,salasana) VALUES ('admin', '$2y$10$uhJE1VMkh5xAjPV.F03NyO7T4zj82raU1bSgjCqwD0iSQkebVAQd.');
 INSERT INTO admin_kayttaja (kayttajanimi,salasana) VALUES ('admin2', '$2y$10$1UqRJskmZJ6fwpU9e0ZNc.pvQIQ57lURGup3rNCQA/jJ0ueLcgFOG');
 
-
+-- Vaihdetaan muiden juomien ja teen järjestystä
+UPDATE tuoteryhma SET trnimi = 'Tee' WHERE trnro = 2;
+UPDATE tuoteryhma SET trnimi = 'Muut juomat' WHERE trnro = 3;
+UPDATE tuoteryhma SET trnimi = 'Teet' WHERE trnro = 2;
+UPDATE tuote SET trnro = 2 WHERE trnro = 3;
+UPDATE tuote SET trnro = 3 WHERE tuotenimi = 'Kaakao' OR tuotenimi =  'Jäävesi' OR tuotenimi = 'Jaffa' OR tuotenimi = 'Cola' OR tuotenimi = 'Omenamehu' OR tuotenimi = 'Appelsiinimehu';
